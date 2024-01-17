648463c98925cb44da71efbe9bbfa0efec9ec747
pool2win <pool2win@users.noreply.github.com>
2024-01-17 15:19:11 +0100

    Add tests for CLI

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -16,3 +16,17 @@ pub struct Cli {
     #[arg(long)]
     pub addpeer: Option<Vec<String>>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_should_build_struct_for_parsing_cli_options() {
+        let _ = env_logger::try_init();
+        let options = vec!["node", "--bind=localhost:25188", "--addpeer=1.2.3.4:8080"];
+        let args = Cli::try_parse_from(options).expect("Error parsing options");
+        assert_eq!(args.bind, "localhost:25188");
+        assert_eq!(args.addpeer, Some(vec!["1.2.3.4:8080".to_string()]));
+    }
+}
